a7e91dce48c04a8c3b61c08f353612300a5bf597
Fletcher Foti <ffoti@berkeley.edu>
2013-10-19 18:38:18 -0700

    adding data for other regions

--- go.py
+++ go.py
@@ -1,16 +1,29 @@
 import pandas as pd
 
-df = pd.read_csv('bats2013MTC_activities.csv')
+df = pd.read_csv('bats2013_activities.csv',index_col=["SAMPN","PERNO"])
 
 df['MODE'] = df.MODE.map({1:3,2:2,3:4,4:4,5:5,6:5,7:5,8:4,9:5,10:5,11:1,12:4,13:1,14:1,15:1,16:1,17:1,18:1,19:1,20:1,21:1,22:1,23:1,24:1,25:1,26:1,27:1,28:1,29:1})
 
-#df = df[(df.HCITY == "SAN FRANCISCO")+(df.WCITY == "SAN FRANCISCO")]
-df = df[['SAMPN','PERNO','APURP1','MODE','ARR_HR','XCORD','YCORD','INCOM','AGE']]
-#df = df.dropna(how='any')
+df = df[['INCOM','AGE','AREA','MODE','ARR_HR','XCORD','YCORD']]
 df.MODE = df.MODE.fillna(6)
 df = df[df.INCOM<=10]
 df = df[df.AGE<=100]
 print df.describe()
+print df.AREA.value_counts()
 
-for name, group in df.groupby('INCOM'):
-    group.to_csv('minibats%s.csv'%name,index=False)
+regions = {
+22: "bayarea",
+30: "losangeles",
+25: "sacramento",
+28: "sandiego",
+3:  "monterey",
+12: "bakersfield",
+8:  "fresno"
+}
+
+for region in regions.items():
+  tmpdf = df[df.AREA == region[0]]
+  del tmpdf["AREA"]
+  for name, group in tmpdf.groupby('INCOM'):
+    del group["INCOM"]  
+    group.to_csv('%s/minibats%s.csv'%(region[1],name),index_label=['HHID','PERID'])
