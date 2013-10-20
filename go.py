74e7f197ee86b509f9557f8c8cf1576bda6ed8af
Fletcher Foti <ffoti@berkeley.edu>
2013-10-19 18:44:19 -0700

    adding noise to data

--- go.py
+++ go.py
@@ -1,9 +1,11 @@
-import pandas as pd
+import pandas as pd, numpy as np
 
 df = pd.read_csv('bats2013_activities.csv',index_col=["SAMPN","PERNO"])
 
 df['MODE'] = df.MODE.map({1:3,2:2,3:4,4:4,5:5,6:5,7:5,8:4,9:5,10:5,11:1,12:4,13:1,14:1,15:1,16:1,17:1,18:1,19:1,20:1,21:1,22:1,23:1,24:1,25:1,26:1,27:1,28:1,29:1})
 
+df.XCORD += np.random.random(len(df.index))*.0005
+df.YCORD += np.random.random(len(df.index))*.0005
 df = df[['INCOM','AGE','AREA','MODE','ARR_HR','XCORD','YCORD']]
 df.MODE = df.MODE.fillna(6)
 df = df[df.INCOM<=10]
